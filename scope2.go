b28ec9fc8a86683e5bedcad9908320b9f818a05e
Bruce Ediger <bediger8@gmail.com>
2021-01-28 09:12:52 -0700

    Correct a comment

--- scope2.go
+++ scope2.go
@@ -11,10 +11,10 @@ func xlist(List *list.List) {
 List:
 	for List.List != nil {
 		/*
-		* There are 5, almost 6, things named "List" in this scope:
+		* There are 4, almost 5, things named "List" in this scope:
 		* 1. type list.List
 		* 2. element List of type List
-		* 3. label .ist
+		* 3. label List
 		* 4. shadow variable List.
 		* and almost, the formal argument variable named List
 		 */
